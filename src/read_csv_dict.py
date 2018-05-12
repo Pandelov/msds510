ab23f56314010ecb995f922752b3b5fd04fd24c3
Stan Pandelov <spandelov@my.bellevue.edu>
2018-05-12 14:14:31 -0500

    Fix code style issues - in progress

--- src/read_csv_dict.py
+++ src/read_csv_dict.py
@@ -1,20 +1,21 @@
 import sys
-import csv                                                 #import the csv module
+import csv                                                 # import the csv module
 
 
-def main():                                                #if there is more than one argument, then we need to say that
-    if (len(sys.argv) != 2):                               #the converter takes one parameter as an input file
+def main():                                                # if there is more than one argument, then we need to say that
+    if (len(sys.argv) != 2):                               # the converter takes one parameter as an input file
         print("this converter takes one parameters, "
               "an input file")
     else:
-        print("input file: " + sys.argv[1])                #if false make sure than evetything runs ok
+        print("input file: " + sys.argv[1])                # if false make sure than evetything runs ok
         readCSVFile(sys.argv[1])
 
 
 def readCSVFile(infile):
 
     with open(infile, 'rU') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')    #using the dictionary reader
+        # using the dictionary reader
+        reader = csv.DictReader(csvfile, delimiter=',')
         data = list(reader)
         print(data[160])
 
