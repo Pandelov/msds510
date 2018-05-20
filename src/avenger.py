8d7eb535552ad1be906a744e6436f1f0a81d9e1f
Stan Pandelov <spandelov@my.bellevue.edu>
2018-05-20 10:35:20 -0500

    OOP

--- src/avenger.py
+++ src/avenger.py
@@ -14,6 +14,38 @@ class Avenger:
     def name_alias(self):
         return self.record.get('name_alias')
 
+    def appearances(self):
+        return int(self.record.get('appearances'))
+
+    def is_current(self):
+        if self.record.get('is_current')==YES
+            return true
+        else
+            return false
+
+    def gender(self):
+        return self.record.get('gender')
+
+    def year(self):
+        return int(self.record.get('year'))
+
+    def date_joined(self):
+
+    def days_since_joining(self):
+        return int(self.record.get('days_since_joining'))
+
+    def years_since_joining(self):
+        return int(self.record.get('years_since_joining'))
+
+    def notes(self):
+        return self.record.get('notes')
+
+    def _str_(self):
+        return self.record.get('_str_')
+
+    def _repr_(self):
+        return self.record.get('_repr_')
+
 
 
 
