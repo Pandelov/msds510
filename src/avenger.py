c0aefc313023ebdc70c865e617c9a7efb339e4c6
Stan Pandelov <spandelov@my.bellevue.edu>
2018-06-02 17:49:46 -0500

    Adding docstring to avenger.py

--- src/avenger.py
+++ src/avenger.py
@@ -1,40 +1,78 @@
 class Avenger:
     def _init_(self, record):
         """
-        Initializes the object with a dictionary-based record.
+        Initializes the object
 
         Args:
-            record (dict) : Dictionary-based record of Avenger data
+            record (dict) : Dictionary-based record
         """
         self.record = record
 
     def url (self):
+        """
+        Returns:
+            str:URL
+        """
         return self.record.get('url')
 
     def name_alias(self):
+         """
+        Returns:
+            str:Name or alias of the character
+        """
         return self.record.get('name_alias')
 
     def appearances(self):
+        """
+        Returns:
+            int: How many times that character
+            appears
+        """
         return int(self.record.get('appearances'))
 
     def is_current(self):
+        """
+        Returns:
+            bool: Is it still active? (True/False)
+        """
+
         if self.record.get('is_current')==YES
             return true
         else
             return false
 
     def gender(self):
+         """
+        Returns:
+            str: The gender of the character
+        """
         return self.record.get('gender')
 
     def year(self):
+        """
+        Returns:
+            int: The year when the character became a member of the Avengers
+        """
         return int(self.record.get('year'))
 
     def date_joined(self):
+        """
+        Returns:
+            datetime.date: The date the character joined the Avengers
+        """
 
     def days_since_joining(self):
+        """
+        Returns:
+            int: The number of days since the character joined
+        """
         return int(self.record.get('days_since_joining'))
 
     def years_since_joining(self):
+        """
+        Returns:
+            int: The number of years since the character joined
+        """
         return int(self.record.get('years_since_joining'))
 
     def notes(self):
